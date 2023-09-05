2313196ba22b54c5947c0a7471446f28439afb31
yassine <yeladlouni@gmail.com>
2023-09-05 10:49:51 +0100

    confluent

--- src/main/java/ProducerConfluentExample.java
+++ src/main/java/ProducerConfluentExample.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class ProducerConfluentExample {
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class ProducerConfluentExample {
+    public ProducerConfluentExample() throws IOException {
+        final Properties props = loadConfig("/Users/yassine/kafka/config/client.properties");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "my-app");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        String topic = "orders";
+
+        for (int i=0; i < 1000000; i++) {
+            String key = "key-" + i;
+            String value = "value-" + i;
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+            producer.send(record);
+        }
+
+        producer.close();
+    }
+
+    public static Properties loadConfig(final String configFile) throws IOException {
+        if (!Files.exists(Paths.get(configFile))) {
+            throw new IOException(configFile + " not found.");
+        }
+        final Properties cfg = new Properties();
+        try (InputStream inputStream = new FileInputStream(configFile)) {
+            cfg.load(inputStream);
+        }
+        return cfg;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ProducerConfluentExample();
+    }
 }
